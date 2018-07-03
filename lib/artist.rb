41bba70500145cbb7938d6511926658d668124ed
Valerie Moore <periwinkle17@outlook.com>
2018-07-03 15:46:18 +0000

    automatically backed up by learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -36,7 +36,9 @@ class Artist
   def self.print_songs
     #"Dirty Diana\nBillie Jean\n"
     @@all.each do |artist| artist.songs
-    
+      @songs.each do |song| puts "song\n"
+      end
+    end
   end
   
 end
