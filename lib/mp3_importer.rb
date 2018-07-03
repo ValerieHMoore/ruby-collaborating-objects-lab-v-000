3347b1901655b38e478a9feda291e0d1357f456a
Valerie Moore <periwinkle17@outlook.com>
2018-07-03 16:23:33 +0000

    automatically backed up by learn

--- lib/mp3_importer.rb
+++ lib/mp3_importer.rb
@@ -1,8 +1,12 @@
 require 'pry'
 
 class MP3Importer
-  def import(list_of_filenames)
-    list_of_filenames.each{ |filename| Song.new_by_filename(filename) }
+  
+  # def import(list_of_filenames)
+  #   list_of_filenames.each{ |filename| Song.new_by_filename(filename) }
+  # end
+  def initialize(path)
+    path = self.new
   end
 
 end
