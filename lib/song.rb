490b088ef3d96a7fa2ebb3acbcff965531bd12fc
Valerie Moore <periwinkle17@outlook.com>
2018-07-03 17:05:56 +0000

    automatically backed up by learn

--- lib/song.rb
+++ lib/song.rb
@@ -10,7 +10,7 @@ class Song
   def self.new_by_filename(filename)
     song = self.new(filename.split(" - ")[1])
     song.artist = Artist.find_or_create_by_name(filename.split(" - ")[0])
-    song.artist 
+    song.artist.add_song
     binding.pry
     song
   end
